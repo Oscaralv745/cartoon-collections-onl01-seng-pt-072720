51e90a3d877f97df4b27ddf2c2596fc7d81934ed
Oscar Alvarez <gavinalv6@gmail.com>
2020-08-03 17:06:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,11 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  if array.any? {|i| 
+  if array.any? {|i| i.length > 4}
+    return true 
+  else 
+    false 
+  end
 end
 
 def find_the_cheese# code an argument here
