4fa11ed361f31a45b76969f2e529215826057b5f
Oscar Alvarez <gavinalv6@gmail.com>
2020-08-03 16:37:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,5 @@
 def roll_call_dwarves(dwarves)
-  i = 0 
-  new_dwarves = []
-  while i < dwarves.length 
-    new_dwarves << yield(dwarves[i])
-    i += 1 
-  end
-  new_dwarves
+ 
 end
 
 def summon_captain_planet# code an argument here
