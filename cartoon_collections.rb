663f0bd0ea6bc7ff7515ae9c4a43dea323b9d9df
Oscar Alvarez <gavinalv6@gmail.com>
2020-08-03 17:10:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,9 +28,5 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if array.include? {|cheese_types|}
-    return true
-  else
-    false
-  end
+  
 end
